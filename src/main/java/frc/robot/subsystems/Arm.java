9b34a5c872032b1a4d1151f14e9e391ad89c5922
brian ely <brian.h.ely@gmail.com>
2023-01-23 20:37:47 -0600

    add stubs for subsystems

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+  // motors
+
+  public Arm() {
+    // instantiate the motors
+  }
+
+  public void rotate(double angle) {
+
+  }
+
+  public void rotateTo(double angle) {
+
+  }
+  
+}
