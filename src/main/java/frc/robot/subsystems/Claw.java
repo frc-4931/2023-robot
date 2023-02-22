5b988afb4e701e5b8ffef27b2662b1ed95bc9d31
brian ely <brian.h.ely@gmail.com>
2023-02-21 21:30:49 -0600

    fix channels

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -7,11 +8,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Claw extends SubsystemBase {
+  private Compressor compressor = new Compressor(0, PneumaticsModuleType.CTREPCM);
   private DoubleSolenoid rod1, rod2;
 
   public Claw() {
-    rod1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 5, 4);
-    rod2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 7, 6);
+    rod1 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 2, 4);
+    rod2 = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 3, 5);
   }
   
   public Command openCommand() {
